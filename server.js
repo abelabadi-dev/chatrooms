9bc03ae0b904c36ea140e607c163d912d537a53d
abel abadi <abelabadi2@gmail.com>
2016-07-09 15:31:27 -0500

    server.js
    relocate to correct directory

---
+++ server.js
@@ -0,0 +1,53 @@
+var http = require('http'); //HTTP server and client functionality
+var fs = require('fs'); //filesystem
+var path = require('path'); //filesystem path related functionality
+var mime = require('mime'); //mime type from file extension
+var cache = {}; //storage for cached files
+
+//create http server
+var server = http.createServer(function(request, response) {
+	var filePath = false;
+	if (request.url == '/') {
+		filePath = 'public/index.html';
+	} else {
+		filePath = 'public' + request.url;
+	}
+	var absPath = './' + filePath;
+	serveStatic(response, cache, absPath);
+});
+
+//requested file doesn't exist
+function send404(response) {
+	response.writeHead(404, {'Content-Type': 'text/plain'});
+	response.write('Error 404: resource not found.');
+	response.end();
+}
+//serve file
+function sendFile(response, filePath, fileContents) {
+	response.writeHead(
+		200,
+		{"content-type": mime.lookup(path.basename(filePath))}
+		);
+	response.end(fileContents);
+}
+//check cache, first time read from disk and add to cache
+function serveStatic(response, cache, absPath) {
+	if (cache[absPath]) {
+		sendFile(response, absPath, cache[absPath]);
+	} else {
+		fs.exists(absPath, function(exists) {
+			if (exists) {
+				fs.readFile(absPath, function(err, data) {
+					if (err) {
+						send404(response);
+					} else {
+						cache[absPath] = data;
+						sendFile(response, absPath, data);
+					}
+				});
+			} else {
+				send404(response);
+			}
+		});
+	}
+}
