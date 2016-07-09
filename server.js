36e0b6317ec6fea8095b0ae61ba5f7557d4217e8
abel abadi <abelabadi2@gmail.com>
2016-07-09 15:54:59 -0500

    starting & listening
    starting http server

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ var server = http.createServer(function(request, response) {
 	var absPath = './' + filePath;
 	serveStatic(response, cache, absPath);
 });
-
+//starting http server
+server.listen(3000, function() {
+	console.log("Server listening on port 3000.");
+});
 //requested file doesn't exist
 function send404(response) {
 	response.writeHead(404, {'Content-Type': 'text/plain'});
